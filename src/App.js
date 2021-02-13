06a3c7a533cbfd6cc76658b1f62fc0a4b33be503
sunsun0312 <jessiesun0312@gmail.com>
2021-02-13 11:20:21 +1100

    modify sttyle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Navbar } from 'react-bootstrap';
 import axios from 'axios';
+import moment from 'moment'
 import './App.css';
 
 function App() {
@@ -9,19 +10,33 @@ function App() {
 
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar className="bg-dark justify-content-center" variant="dark">
         <Navbar.Brand>Exchange Rate</Navbar.Brand>
       </Navbar>
       <Card className="card">
-        <div className="row">
-          <label className="currency">BitCoin</label>
-          <input className="rate" type="text" value={amount} placeholder="1"></input>
-        </div>
-        <div className="row">
-          <label className="currency">USD</label>
-          <input className="rate" type="text" value={parseInt(amount) * rate} disabled></input>
-        </div>
-        <Card.Footer>date</Card.Footer>
+        <Card.Body>
+          <div className="row">
+            <div className="col-md-6">
+              <label className="currency">BitCoin</label>
+            </div>
+            <div className="col-md-4">
+              <input className="rate" type="text" value={amount} placeholder="1"></input>
+            </div>
+            
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-md-6">
+              <label className="currency">USD</label>
+            </div>
+            <div className="col-md-4">
+              <input className="rate" type="text" value={parseInt(amount) * rate} disabled></input>
+            </div>
+            
+          </div>
+        </Card.Body>
+        
+  <Card.Footer>Date: {moment().format("DD/MM/YYYY")}</Card.Footer>
       </Card>
       
     </div>
