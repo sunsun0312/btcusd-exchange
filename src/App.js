52690818783cbf24961c8614e2c8d95b249b8677
sunsun0312 <jessiesun0312@gmail.com>
2021-02-13 11:02:32 +1100

    add basic commponents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Card, Navbar } from 'react-bootstrap';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const rate = 4;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Exchange Rate</Navbar.Brand>
+      </Navbar>
+      <Card className="card">
+        <div className="row">
+          <label className="currency">BitCoin</label>
+          <input className="rate" type="text" value={amount} placeholder="1"></input>
+        </div>
+        <div className="row">
+          <label className="currency">USD</label>
+          <input className="rate" type="text" value={parseInt(amount) * rate} disabled></input>
+        </div>
+        <Card.Footer>date</Card.Footer>
+      </Card>
+      
     </div>
   );
 }
